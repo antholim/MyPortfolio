235b4ecf6dad4ca6f5a6e466a773364acf83432d
antholim <139510082+antholim@users.noreply.github.com>
2024-09-17 22:38:24 -0400

    language

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import {useState} from 'react';
 function Header() {
-    const [language, setLanguage] = useState(0);
+    const [language, setLanguage] = useState("");
     const languages = ['English', 'French', 'Spanish'];
+    const handleSelectLanguage = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setLanguage(event.target.value);
+      };
     return (
       <header>
         <span>Anthony Lim</span>
@@ -10,13 +13,14 @@ function Header() {
         <button>Experience</button>
         <button>Contact</button>
         <button>Download CV</button>
-        <select name="select-language" id="select-language">
+        <select name="select-language" id="select-language" onChange={handleSelectLanguage}>
             {languages.map((language, index) => (
-                <option key={index} value={index}>
+                <option key={index} value={language}>
                 {language}
                 </option>
             ))}
         </select>
+        {language && <>{language}</>}
       </header>
     );
   } export default Header;
