3e7ebf014a873c68e394a2a9145c15b43bc80c06
antholim <139510082+antholim@users.noreply.github.com>
2024-09-23 22:27:49 -0400

    MyPortfolio: Update styling for projects and contact me sections

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,7 +7,7 @@ export interface Project {
 export const PROJECTS_ENGLISH:Project[] = [
     {title : "Monster Mini Game", description : "Description", link: ""},
     {title : "Crossword Mini Game", description : "Description", link: ""},
-    {title : "Levenstein Dictionary", description : "Description", link: ""},
+    // {title : "Levenstein Dictionary", description : "Description", link: ""},
     {title : "Cipher Steganography Cracker", description : "Description", link: ""},
     {title : "PaperTrading", description : "Description", link: ""},
 ];
