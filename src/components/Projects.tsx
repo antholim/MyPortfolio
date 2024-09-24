b3b1883102d716b52e9935a1addd7789f0eb2a24
antholim <139510082+antholim@users.noreply.github.com>
2024-09-23 21:36:53 -0400

    header padding

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,9 +4,9 @@ import styles from '../styles/projects.module.css';
 
 function Projects() {
     return (
-        <>
+        <section id="projects">
             <h1>Projects</h1>
-            <section className={styles.projectsGrid} id="projects">
+            <section className={styles.projectsGrid}>
                 {PROJECTS_ENGLISH.map((project: Project, index: number) => {
                     return (
                         <div key={index} className={styles.projectCard}>
@@ -19,6 +19,6 @@ function Projects() {
                     )
                 })}
             </section>
-        </>
+        </section>
     )
 } export default Projects;
