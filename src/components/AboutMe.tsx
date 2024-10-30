b0724a8f34c3a674893de33ae0858dbbf22e127c
antholim <139510082+antholim@users.noreply.github.com>
2024-10-29 22:42:23 -0400

    path fix

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,5 @@
 import { DESCRIPTION } from "../data/aboutme.tsx";
-import styles from "../styles/aboutme.module.css";
+import styles from "../styles/AboutMe.module.css";
 import { LanguageProps } from "../types/props.ts";
 
 function AboutMe({ language }: LanguageProps): JSX.Element {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/header.module.css";
+import styles from "../styles/Header.module.css";
 import { LanguageProps } from "../types/props.ts";
 import {
   navBarLinkEnglish,
