4364904ca4233c77d39f9f8c354b42e8a69abfaf
antholim <139510082+antholim@users.noreply.github.com>
2024-11-17 23:49:37 -0500

    Changed icons

--- src/data/programming.tsx
+++ src/data/programming.tsx
@@ -27,41 +27,42 @@ export interface Tool {
 }
 
 export const LANGUAGES: Tool[] = [
-  { name: "Python", icon: <SiPython /> },
-  { name: "Java", icon: <FaJava /> },
-  { name: "C", icon: <SiC /> },
-  { name: "JavaScript", icon: <SiJavascript /> },
-  { name: "TypeScript", icon: <SiTypescript /> },
-  { name: "HTML", icon: <SiHtml5 /> },
-  { name: "CSS", icon: <SiCss3 /> },
+  { name: "Python", icon: <i className="devicon-python-plain colored"></i> },
+  { name: "Java", icon: <i className="devicon-java-plain colored"></i> },
+  { name: "C", icon: <i className="devicon-c-plain colored"></i> },
+  { name: "JavaScript", icon: <i className="devicon-javascript-plain colored"></i> },
+  { name: "TypeScript", icon: <i className="devicon-typescript-plain colored"></i>  },
+  { name: "HTML", icon: <i className="devicon-html5-plain colored"></i> },
+  { name: "CSS", icon: <i className="devicon-css3-plain colored"></i> },
+  // { name: "Go", icon: <i className="devicon-go-plain colored"></i> },
 ];
 
 // Frameworks & Libraries
 export const FRAMEWORKS: Tool[] = [
-  { name: "React", icon: <SiReact /> },
-  { name: "Node.js", icon: <FaNode /> },
-  { name: "Express", icon: <SiExpress /> },
+  { name: "React", icon: <i className="devicon-react-plain colored"></i> },
+  { name: "Node.js", icon: <i className="devicon-nodejs-plain colored"></i> },
+  { name: "Express", icon: <i className="devicon-express-original colored"></i>  },
 ];
 
 // Databases
 export const DATABASES: Tool[] = [
-  { name: "MongoDB", icon: <SiMongodb /> },
-  { name: "MySQL", icon: <SiMysql /> },
+  { name: "MongoDB", icon: <i className="devicon-mongodb-plain colored"></i>},
+  { name: "MySQL", icon: <i className="devicon-mysql-plain colored"></i> },
 ];
 
 // Version Control & CI/CD Tools
 export const DEVOPS_TOOLS: Tool[] = [
-  { name: "Git", icon: <FaGit /> },
-  { name: "Github", icon: <FaGithub /> },
-  { name: "Gitlab", icon: <FaGitlab /> },
-  { name: "Docker", icon: <FaDocker /> },
+  { name: "Git", icon: <i className="devicon-git-plain colored"></i> },
+  { name: "Github", icon: <i className="devicon-github-plain colored"></i> },
+  { name: "Gitlab", icon: <i className="devicon-gitlab-plain colored"></i> },
+  { name: "Docker", icon: <i className="devicon-docker-plain colored"></i> },
 ];
 
 // Testing Tools
 export const TESTING_TOOLS: Tool[] = [
-  { name: "Jest", icon: <SiJest /> },
-  { name: "Playwright", icon: <SiPlaywright /> },
-  { name: "Puppeteer", icon: <SiPuppeteer /> },
+  { name: "Jest", icon: <i className="devicon-jest-plain colored"></i> },
+  { name: "Playwright", icon: <i className="devicon-playwright-plain colored"></i> },
+  { name: "Puppeteer", icon: <i className="devicon-puppeteer-plain colored"></i> },
 ];
 
 export const KEY_WORDS = {
